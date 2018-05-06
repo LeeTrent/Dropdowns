200ad35e60ab3777e177fcb0a08bc3f54e63cf6d
LeeTrent <Lee.Trent.1@gmail.com>
2018-05-06 17:08:13 -0500

    Adds CRUD Razor pages and controllers for the Corporation entity

---
+++ Pages/Corporations/Create.cshtml.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Dropdowns.Data;
+using Dropdowns.Models;
+
+namespace Dropdowns.Pages.Corporations
+{
+    public class CreateModel : PageModel
+    {
+        private readonly Dropdowns.Data.DropdownContext _context;
+
+        public CreateModel(Dropdowns.Data.DropdownContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Corporation Corporation { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Corporations.Add(Corporation);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ Pages/Corporations/Edit.cshtml.cs
@@ -0,0 +1,63 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Dropdowns.Data;
+using Dropdowns.Models;
+
+namespace Dropdowns.Pages.Corporations
+{
+    public class EditModel : PageModel
+    {
+        private readonly Dropdowns.Data.DropdownContext _context;
+
+        public EditModel(Dropdowns.Data.DropdownContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Corporation Corporation { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Corporation = await _context.Corporations.SingleOrDefaultAsync(m => m.CorporationID == id);
+
+            if (Corporation == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Corporation).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
